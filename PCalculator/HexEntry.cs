c40d1a42ef4750c3b1c733284d06df4eb5fe8773
Gyomei <xmztei@yahoo.co.jp>
2023-09-04 15:43:38 -0700

    add test code

---
+++ PCalculator/HexEntry.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PCalculator
+{
+    public class GraphicsDrawable : IDrawable
+    {
+        public void Draw(ICanvas canvas, RectF dirtyRect)
+        {
+            canvas.DrawLine(dirtyRect.X, dirtyRect.Y, dirtyRect.X, dirtyRect.Y+20);
+            //canvas.DrawText("test", dirtyRect.X, dirtyRect.Y, dirtyRect.Width,dirtyRect.Height);
+        }
+    }
+
+    class HexEntry 
+    {
+    }
+}
